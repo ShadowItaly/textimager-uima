e3f4d20aff2c5abf2b3c71fe20d3a6b40a84c3bb
Alexander Leonhardt <equinox.salexander@gmail.com>
2021-07-31 14:52:29 +0200

    Added a the config for github actions to continously test the software upon pushing and added some basic test cases

--- textimager-pipeline-annotator/src/main/java/org/hucompute/textimager/PipelineAnnotator.java
+++ textimager-pipeline-annotator/src/main/java/org/hucompute/textimager/PipelineAnnotator.java
@@ -1,8 +1,11 @@
 package org.hucompute.textimager;
 
-public class PipelineAnnotator {
+import org.apache.uima.UIMAException;
+import org.apache.uima.fit.factory.JCasFactory;
+import org.apache.uima.jcas.JCas;
+import org.hucompute.textimager.uima.type.PipelineAnnotation;
 
+public class PipelineAnnotator {
     public static void main(String []args) {
-        System.out.println("Hello World!");
     }
 }
--- textimager-pipeline-annotator/src/test/java/org/hucompute/textimager/TestPipelineAnnotator.java
+++ textimager-pipeline-annotator/src/test/java/org/hucompute/textimager/TestPipelineAnnotator.java
@@ -1,4 +1,32 @@
 package org.hucompute.textimager;
 
+import org.apache.uima.UIMAException;
+import org.apache.uima.fit.factory.JCasFactory;
+import org.apache.uima.fit.util.JCasUtil;
+import org.apache.uima.jcas.JCas;
+import org.junit.Test;
+import org.hucompute.textimager.uima.type.PipelineAnnotation;
+
+import static org.junit.Assert.assertEquals;
+
 public class TestPipelineAnnotator {
+    @Test
+    public void test_type_available() throws UIMAException {
+        JCas jcas = JCasFactory.createJCas();
+        jcas.setDocumentLanguage("de");
+        jcas.setDocumentText("Sein oder nicht sein das ist hier die Frage.");
+        //Just check that the type exists and we can query from the jCAS object
+        PipelineAnnotation pipe = new PipelineAnnotation(jcas);
+        pipe.setAnnotator_name("spacy_multi_tagger");
+        pipe.setSettings("{}");
+        pipe.addToIndexes();
+
+        int count = 0;
+        for(PipelineAnnotation x : JCasUtil.select(jcas,PipelineAnnotation.class)) {
+            assertEquals(x.getAnnotator_name(),"spacy_multi_tagger");
+            assertEquals(x.getSettings(),"{}");
+            count+=1;
+        }
+        assertEquals(count,1);
+    }
 }
