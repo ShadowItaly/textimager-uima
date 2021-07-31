d0b595cb1d75c5853ffff4f1585c1a4794058de8
Alexander Leonhardt <equinox.salexander@gmail.com>
2021-07-31 13:47:57 +0200

    Updated the type system to include the new PipelineAnnotation Type and added the code base for the pipeline annotator

---
+++ textimager-pipeline-annotator/src/main/java/org/hucompute/textimager/PipelineAnnotator.java
@@ -0,0 +1,8 @@
+package org.hucompute.textimager;
+
+public class PipelineAnnotator {
+
+    public static void main(String []args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ textimager-pipeline-annotator/src/test/java/org/hucompute/textimager/TestPipelineAnnotator.java
@@ -0,0 +1,4 @@
+package org.hucompute.textimager;
+
+public class TestPipelineAnnotator {
+}
